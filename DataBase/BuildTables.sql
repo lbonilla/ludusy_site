f5b7035d943b712c3f655869ad12b258aac4d024
Luis Bonilla Hernandez <luisbonillah@gmail.com>
2013-03-04 10:51:10 -0600

    Init site files

---
+++ DataBase/BuildTables.sql
@@ -0,0 +1,9 @@
+create table Post(ID Integer, Content  Varchar(800));
+
+create table Page(ID Integer, Content Varchar(800))	;
+
+create table PagePost(PageID Integer, PostID Integer);
+
+create table Category(ID Integer, Name Integer);	
+
+create table PostPost(IDPost1 Integer, IDPost2 Integer);
